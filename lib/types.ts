128a0263c7a483e092dedf49b93e9cebcbcd0290
linhieng <790728740@qq.com>
2023-09-05 10:16:35 +0800

    5.1-over 安装依赖、导入 utils.ts、更新 types.ts
    npm i -S lodash.union@4.6.0 jsonpointer@4.1 json-schema-merge-allof@0.7

--- lib/types.ts
+++ lib/types.ts
@@ -9,30 +9,48 @@ export enum SchemaTypesEnum {
     'BOOLEAN' = 'boolean',
 }
 
-// TODO: 举例说明
+// $ref 表示引用自其他的 schema。
+// 比如 $ref: '#/defined/xxx' 表示该 Schema 的定义引用自动 '#/defined/xxx' 位置的定义。
+// # 表示根目录。每一个 Schema 都有一个路径。
 type SchemaRef = {
     $ref: string
 }
 
 export interface Schema {
-    type: SchemaTypesEnum | string
+    type?: SchemaTypesEnum | string
     const?: any
     format?: string
+
+    title?: string
     default?: any
+
     properties?: {
         [key: string]: Schema | { $ref: string }
     }
     items?: Schema | Schema[] | SchemaRef
+    uniqueItems?: any
     dependencies?: {
         [key: string]: string[] | Schema | SchemaRef
     }
     oneOf?: Schema[]
+    anyOf?: Schema[]
+    allOf?: Schema[]
+    // TODO: uiSchema
     // vjsf?: VueJsonSchemaConfig
     required?: string[]
     enum?: any[]
+    enumNames?: any[]
     enumKeyValue?: any[]
     additionalProperties?: any
     additionalItems?: Schema
+
+    minLength?: number
+    maxLength?: number
+    minimun?: number
+    maximum?: number
+    multipleOf?: number
+    exclusiveMaximum?: number
+    exclusiveMinimum?: number
 }
 
 export const FieldPropsDefine = {
